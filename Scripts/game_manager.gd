42714d1d682e9635b706bfedffef90ad00d0c0d2
ezekiel342007 <ezekiel342007@gmail.com>
2024-06-26 19:06:12 +0100

    added global script and localized the default clear colour of test_level.tscn

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	RenderingServer.set_default_clear_color(Color(0.46, 0.00, 0.56, 1.00))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
