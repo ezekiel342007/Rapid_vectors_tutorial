22603b19fb2d1cd3235b756e831f85c9f368a547
ezekiel342007 <ezekiel342007@gmail.com>
2024-07-01 17:21:44 +0100

    made state nodes and designed state machine of enemy dino, working on state machine controller of eney dino

--- Enemies/dino/Gravity.gd
+++ Enemies/dino/Gravity.gd
@@ -5,11 +5,6 @@ extends Node
 
 const GRAVITY: int = 1000
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if !character_body_2d.is_on_floor():
---
+++ Scripts/state_machine/node_finite_state_machine.gd
@@ -0,0 +1,50 @@
+class_name NodeFiniteStateMachine
+extends Node
+
+@export var initial_node_state: NodeState
+
+var node_states: Dictionary = {}
+var current_node_state: NodeState
+var current_node_state_name: String
+
+
+func _ready():
+	for child in get_children():
+		if child is NodeState:
+			node_states[child.name.to_lower()] = child
+
+		# If there was an initial node state make the current node state that state
+		if initial_node_state:
+			initial_node_state.enter()
+			current_node_state = initial_node_state
+
+
+func _process(delta: float):
+	if current_node_state:
+		current_node_state.on_process(delta)
+
+
+func _physics_process(delta: float):
+	if current_node_state:
+		current_node_state.on_physics_process(delta)
+
+	print("State:", current_node_state_name.to_lower())
+	
+func transition_to(node_state_name: String):
+	# Checks if the given state is already the current state
+	if node_state_name == current_node_state.name.to_lower():
+		return
+
+	# If not we make a new state determined by the given state name
+	var new_node_state = node_states.get(node_state_name.to_lower())
+
+	if !new_node_state:
+		return
+
+	# Exit the current state to enter the new one
+	if current_node_state:
+		current_node_state.exit()
+
+	new_node_state.enter()
+	current_node_state = new_node_state
+	current_node_state_name = current_node_state.name.to_lower()
