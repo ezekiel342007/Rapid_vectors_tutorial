f1c71558b1cc7242905c1f1f549799fec48c7177
ezekiel342007 <ezekiel342007@gmail.com>
2024-07-05 17:35:14 +0100

    started making collectibles

---
+++ Collectibles/Blue_coin/blue_coin.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var award_amount: int = 1
+
+@onready var label = $Label
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+func _ready():
+	label.hide()
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		print(award_amount)
+
+	animated_sprite_2d.hide()
+
+	label.text = "%s" % award_amount
+	label.show()
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(label, "position", Vector2(label.position.x, label.position.y + -10), 0.5).from_current()
