0d5e0e6f6495a8c8d011eb458d63bf1d46aa7631
ezekiel342007 <ezekiel342007@gmail.com>
2024-07-07 19:50:37 +0100

    made pause and main menu

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,11 +1,17 @@
 extends Node
 
+var level_1 = preload("res://levels/level_1.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	RenderingServer.set_default_clear_color(Color(0.46, 0.00, 0.56, 1.00))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func start_game():
+	transition_to_scene(level_1.resource_path)
+
+
+func transition_to_scene(scene_path):
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_file(scene_path)
