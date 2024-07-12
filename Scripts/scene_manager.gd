93bb0cfabe513e5df569be031f47d975fd17fe22
ezekiel342007 <ezekiel342007@gmail.com>
2024-07-12 21:05:06 +0100

    designed transition screen shader and applied transition screen

--- Scripts/scene_manager.gd
+++ Scripts/scene_manager.gd
@@ -5,10 +5,14 @@ var scenes: Dictionary = {
 	"level2": "res://levels/level_2.tscn"
 }
 
+@onready var scene_transition_screen = preload("res://ui/Screen_transition/scene_transition_screen.tscn")
 
 func transition_to_scene(level: String):
 	var scene_path: String = scenes.get(level)
 
 	if scene_path != null:
-		await get_tree().create_timer(1.0).timeout
+		var scene_transition_screen_instance = scene_transition_screen.instantiate()
+		get_tree().get_root().add_child(scene_transition_screen_instance)
+		await get_tree().create_timer(5.0).timeout
 		get_tree().change_scene_to_file(scene_path)
+		scene_transition_screen_instance.queue_free()
