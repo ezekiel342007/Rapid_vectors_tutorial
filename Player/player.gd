179d3e8af2368d555d70d1aef7baf78a1909cbad
ezekiel342007 <ezekiel342007@gmail.com>
2024-07-14 22:46:30 +0100

    adjusting player to use a state machine

--- Player/player.gd
+++ Player/player.gd
@@ -4,121 +4,6 @@ var bullet = preload("res://Player/bullet.tscn")
 var player_death_effect = preload("res://Player/player_death_effect/player_death_effect.tscn")
 @onready var muzzle: Marker2D = $Muzzle
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var hit_animation_player = $HitAnimationPlayer
-
-
-@export var speed: int = 1000
-@export var jump: int = -300
-@export var slow_down_speed: int = 1700
-@export var max_horizontal_speed: int =  300 
-@export var jump_horizontal_speed: int = 1000
-@export var max_jump_horizontal_speed: int =  300 
-@export var jump_count: int = 4
-
-const GRAVITY = 1000
-
-var muzzle_position
-var current_state: State
-var current_jump_count: int
-
-enum State { Idle, Run, Jump, Shoot }
-
-
-func _ready():
-	current_state = State.Idle
-	muzzle_position = muzzle.position
-
-
-func _physics_process(delta: float):
-	player_falling(delta)
-	player_idle()
-	player_run(delta)
-	player_jump(delta)
-	player_muzzle_position()
-	player_shooting()
-
-	move_and_slide()
-	player_animations()
-
-	# print("State: ", State.keys()[current_state])
-
-
-func player_falling(delta: float):
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-	
-
-func player_idle():
-	if is_on_floor():
-		current_state = State.Idle
-
-
-func player_run(delta: float):
-	if !is_on_floor():
-		return
-	
-	var direction = import_movement()
-	if direction:
-		velocity.x += direction * speed * delta
-
-		# Stops velocity.x from going above max_horizontal_speed
-		velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
-	else:
-		velocity.x = move_toward(velocity.x, 0, slow_down_speed * delta)
-
-	if direction != 0:
-		current_state = State.Run
-		animated_sprite_2d.flip_h = false if direction > 0 else true
-
-
-func player_jump(delta: float):
-	var jump_input: bool = Input.is_action_just_pressed("jump")
-	if is_on_floor() && jump_input:
-		current_jump_count = 0
-		velocity.y = jump
-		current_jump_count += 1
-		current_state = State.Jump
-
-	if !is_on_floor() && jump_input && current_jump_count < jump_count:
-		velocity.y = jump
-		current_jump_count += 1
-		current_state = State.Jump
-
-	if !is_on_floor() and current_state == State.Jump:
-		var direction = import_movement()
-		velocity.x += direction * jump_horizontal_speed * delta
-		velocity.x = clamp(velocity.x, -max_jump_horizontal_speed, max_jump_horizontal_speed)
-
-
-func player_shooting():
-	var direction = import_movement()
-
-	if direction != 0 and Input.is_action_just_pressed("shoot"):
-		var bullet_instance = bullet.instantiate() as Node2D
-		bullet_instance.direction = direction
-		bullet_instance.global_position = muzzle.global_position
-		get_parent().add_child(bullet_instance)
-		current_state = State.Shoot
-
-
-func player_muzzle_position():
-	var direction = import_movement()
-	if direction > 0:
-		muzzle.position.x = muzzle_position.x
-	elif direction < 0:
-		muzzle.position.x = -muzzle_position.x
-
-
-func player_animations():
-	if current_state == State.Idle:
-		animated_sprite_2d.play("idle")
-	elif current_state == State.Run and animated_sprite_2d.animation != "run_and_shoot" :
-		animated_sprite_2d.play("run")
-	elif current_state == State.Jump:
-		animated_sprite_2d.play("jump")
-	elif current_state == State.Shoot:
-		animated_sprite_2d.play("run_and_shoot")
-
 
 func player_death():
 	var player_death_instance = player_death_effect.instantiate() as Node2D
@@ -127,15 +12,13 @@ func player_death():
 	queue_free()
 
 
-func import_movement():
-	var direction: float = Input.get_axis("move_left", "move_right")
-	return direction
-
-
 func _on_hurtbox_body_entered(body: Node2D):
 	if body.is_in_group("Enemy"):
-		# print("Enemy entered: ", body.damage_amount)
-		hit_animation_player.play("hit")
+		print("Enemy entered: ", body.damage_amount)
+		var tween = get_tree().create_tween()
+		tween.tween_property(animated_sprite_2d, "material:shader_parameter/enabled", true, 0)
+		tween.tween_property(animated_sprite_2d, "material:shader_parameter/enabled", false, 0.2)
 		HealthManager.decrease_health(body.damage_amount)
+
 	if HealthManager.current_health == 0:
 		player_death()
