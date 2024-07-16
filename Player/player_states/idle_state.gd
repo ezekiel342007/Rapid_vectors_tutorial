d86da9c99a3fe42438d68e7b2b76c641bd625d64
ezekiel342007 <ezekiel342007@gmail.com>
2024-07-16 22:42:25 +0100

    implemented breaking system while entering idle mode

--- Player/player_states/idle_state.gd
+++ Player/player_states/idle_state.gd
@@ -3,12 +3,18 @@ extends NodeState
 @export var character_body_2d: CharacterBody2D
 @export var animated_sprite_2d: AnimatedSprite2D
 
+@export_category("Physics Friction")
+@export var slow_down_speed: int = 1500
+
 
 func on_process(delta: float):
 	pass
 
 	
 func on_physics_process(delta: float):
+	character_body_2d.velocity.x = move_toward(character_body_2d.velocity.x, 0, slow_down_speed)
+
+	character_body_2d.move_and_slide()
 	# transmitting states
 
 	# fall state
